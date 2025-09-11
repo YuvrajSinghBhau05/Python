6811d30c36f370ff71b9c2df0d61c33c9f38c20b
YuvrajSinghBhau05 <yuvrajsinghbhau.g.41@gmail.com>
2025-09-11 11:26:04 +0530

    Add files via upload

---
+++ practice6.py
@@ -0,0 +1,11 @@
+num=int(input("Enter the number: "))
+if num>0:
+    if num%2==0:
+        print('number is +ve and even')
+    else:
+        print("number is +ve and odd")
+else:
+    if num%2==0:
+        print("number is -ve and even")
+    else:
+        print("number is -ve and odd")
